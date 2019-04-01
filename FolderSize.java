aae96f56b691dfa470c30f6f13a54d96275f9646
srivallikomma <49111660+srivallikomma@users.noreply.github.com>
2019-04-01 17:43:16 +0530

    Update FolderSize.java

--- FolderSize.java
+++ FolderSize.java
@@ -1,33 +1,33 @@
-package defaultpackage;
+package casestudy;
+
 import java.io.*;
 
-public class FolderSize {
-
-	public static void main(String[] args) {
-		   try {
-		    	
-		   FolderSize size = new FolderSize();
-        BufferedReader reader =  
-                new BufferedReader(new InputStreamReader(System.in)); 
-           System.out.println("Enter path : ");
-     String folder = reader.readLine();
-    
-        long fileSizeByte = size.getFolderSize(new File(folder));
+public class DiskSpace {
+	public static void main(String args[]) {
+		try {
+			DiskSpace space= new DiskSpace();
+			BufferedReader reader= new BufferedReader(new InputStreamReader(System.in));
+			System.out.print("Enter path: ");
+			String filesize =reader.readLine();
+			long spaceinfolder=space.getsize(new File (filesize));
+			//System.out.println("spaceinfolder :" + spaceinfolder);
+		}
+		catch (Exception e) {}
 	}
-        catch (Exception e) {
-        	}
-        }
-	
-		    public static long getFolderSize(File dir) {
-		        long size = 0;
-		        for (File file : dir.listFiles()) {
-		            if (file.isFile()) {
-		                System.out.println(dir +"\\" + file.getName() + " " + file.length());
-		                size += file.length();
-		            }
-		            else
-		                size += getFolderSize(file);
-		        }
-		        return size;
-		    }
+
+	public long getsize(File folder) {
+		long length=0;
+		for(File file:folder.listFiles()) {
+			if(file.isFile()) {
+				System.out.println(folder+ "\\"+ file.getName()+ " "+file.length());
+				 length = file.length();
+			}
+			else
+				length +=getsize(file);
+			
+		}
+		
+		return length;
+	}
+
 }
